6b7f0b99b6cbc2822474cecd1aacb3e03d294bac
iberniex <ben.karanja@icloud.com>
2025-07-03 16:05:18 +0300

    feat(elixir): meta-programming(macros, quote/unquote)

---
+++ Experiments/elixir/macros.ex
@@ -0,0 +1,13 @@
+defmodule Hello do
+  defmacro __using__(opts) do
+    greeting = Keyword.get(opts, :greeting, "Hi")
+
+    quote do
+      def hello(name), do: unquote(greeting) <> ", " <> name
+    end
+  end
+end
+
+defmodule Example do
+  use Hello, greeting: "hello"
+end
