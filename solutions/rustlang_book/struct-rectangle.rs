94d05bdb92b886bb0e00105fb3d407d8a1c8d81b
iberniex <ben.karanja@icloud.com>
2026-08-05 13:36:50 +0300

    feat(rust): implementing methods in structs
    adding more knowledge to structs in using structs and the impl keyword
    in implementing struct methods to be used in rust

--- solutions/rustlang_book/struct-rectangle.rs
+++ solutions/rustlang_book/struct-rectangle.rs
@@ -3,6 +3,17 @@ struct Rectangle {
     width: u64,
     length: u64,
 }
+
+impl Rectangle {
+    fn area(&self) -> u64 {
+        self.width * self.length
+    }
+
+    fn compare(&self, other: &Rectangle) -> bool {
+        self.length < other.length && self.width < other.width
+    }
+}
+
 fn main() {
     // refactoring
     // let width = 30;
@@ -16,20 +27,17 @@ fn main() {
         length: 50,
     };
 
-    let rectl = Rectangle {
+    let rect2 = Rectangle {
         width: dbg!(40 * 50),
         length: 50,
     };
 
+    println!("rect1 compared to rect2 {}", &rect1.compare(&rect2));
+
     println!(
         "The area of the rectangle is {} square pixels",
-        area(&rect1)
+        rect1.area()
     );
 
-    // println!("rectl is {rect1:#?}");
-    dbg!(&rectl);
-}
-
-fn area(rectangle: &Rectangle) -> u64 {
-    rectangle.width * rectangle.length
+    println!("rectl is {rect1:#?}");
 }
