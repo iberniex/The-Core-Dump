beffdc87a89d0abc3259167e5484318b258fd502
iberniex <ben.karanja@icloud.com>
2026-08-10 16:42:39 +0300

    feat(rust): refactoring of factorial computation in rust using the `.product()` method

--- solutions/mathematical-thinking-in-computer-science/factorial.rs
+++ solutions/mathematical-thinking-in-computer-science/factorial.rs
@@ -4,8 +4,16 @@ fn main() {
         Some(val) => println!("The value is {}", val),
         None => println!("The value is not found oops!"),
     }
+
+    let factorial_cleany = factorial_clean(5);
+
+    println!("The clean factorial suggested by Clippy is {factorial_cleany}")
 }
 
 fn factorial(n: u32) -> Option<u32> {
     (1..=n).try_fold(1u32, |acc, x| acc.checked_mul(x))
+}
+
+fn factorial_clean(n: u32) -> u32 {
+    (1..=n).product()
 }
