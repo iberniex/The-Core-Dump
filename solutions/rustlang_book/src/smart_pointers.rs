49ec46de411a4c83e64234cca5f0f7d392388981
iberniex <ben.karanja@icloud.com>
2026-08-13 13:53:17 +0300

    feat(rust): Drop trait implementation
    Doing a form of computation before the structure has being dropped
    during compile time. Using in files and networks after the consumation
    of the solution and doing something before the consumation process.

--- solutions/rustlang_book/src/smart_pointers.rs
+++ solutions/rustlang_book/src/smart_pointers.rs
@@ -9,6 +9,7 @@ impl<T> MyBox<T> {
     }
 }
 
+// Implementation for the Deference trait on MyBox
 impl<T> Deref for MyBox<T> {
     type Target = T;
 
@@ -17,10 +18,34 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+// Drop Trait
+// Implementation of the Drop Trait on a structure CustomSmartPointer
+
+#[derive(Debug)]
+struct CustomSmartPointer {
+    string: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data {}", self.string)
+    }
+}
+
 pub fn testing_box_smart_pointer() {
     let s = MyBox::new(5);
 
+    let r = Box::new(6);
+
     println!("{:?}", s);
+    println!("{:?}", r);
 
     println!("{}", *s);
+    println!("{}", *r);
+
+    let s = CustomSmartPointer {
+        string: String::from("Ready"),
+    };
+
+    println!("Smart Pointer {s:?} Created")
 }
