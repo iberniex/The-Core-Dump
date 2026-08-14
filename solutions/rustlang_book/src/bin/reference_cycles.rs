aa5584240ed13c9fe6f6b5b8c8ec5d1b28a50f8c
iberniex <ben.karanja@icloud.com>
2026-08-14 15:00:18 +0300

    feat(rust): learning on reference cycles
    This are logic bugs that are not caught during compile time but happen
    in runtime and they should be used sparringly cause in this case we use
    a cons list of a and has a reference to Nil then change it to reference
    b which also references a and leads to infinite recurrence leading to
    the code being run in the commented section below to overflow at
    runtime.

---
+++ solutions/rustlang_book/src/bin/reference_cycles.rs
@@ -0,0 +1,41 @@
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            List::Cons(_, value) => Some(value),
+            List::Nil => None,
+        }
+    }
+}
+fn main() {
+    let a = Rc::new(List::Cons(10, RefCell::new(Rc::new(List::Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(List::Cons(5, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+
+    println!("b next item = {:?}", b.tail());
+
+    // This is the reference cycle
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // This overflows the stack due to the reference in a.tail to be b.tail which leads
+    // to an endless loop
+    // println!("a next item = {:?}", a.tail());
+}
