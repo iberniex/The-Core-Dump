17ac98948f5585ff336bbf88418d753435f983be
iberniex <ben.karanja@icloud.com>
2026-08-17 13:40:54 +0300

    feat(rust): implementing weak<T> in order to prevent reference cycles and how to be used in node implementation

--- solutions/rustlang_book/src/bin/weak_references.rs
+++ solutions/rustlang_book/src/bin/weak_references.rs
@@ -3,12 +3,14 @@ use std::{
     rc::{Rc, Weak},
 };
 
+#[allow(unused)]
 #[derive(Debug)]
 struct Node {
     value: i32,
     parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
+
 fn main() {
     // ready
     let leaf = Rc::new(Node {
@@ -17,13 +19,37 @@ fn main() {
         children: RefCell::new(vec![]),
     });
 
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
-    let branch = Rc::new(Node {
-        value: 5,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    });
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
 
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "Branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+        println!(
+            "Leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+    }
+
     println!("leaf parent = {:#?}", leaf.parent.borrow().upgrade());
+    println!(
+        "Leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
 }
