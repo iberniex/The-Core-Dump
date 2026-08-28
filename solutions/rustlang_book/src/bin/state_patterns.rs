c3f0d801cdfbc1662600feb65afbb85774803015
iberniex <ben.karanja@icloud.com>
2026-08-28 13:18:06 +0300

    feat(rust): suggestions made for adding double approvals and add_text functionality

--- solutions/rustlang_book/src/bin/state_patterns.rs
+++ solutions/rustlang_book/src/bin/state_patterns.rs
@@ -12,7 +12,7 @@ impl Post {
     }
 
     pub fn add_text(&mut self, text: &str) {
-        self.content.push_str(text);
+        self.state.as_ref().unwrap().add_text(&self.content, text);
     }
 
     pub fn content(&self) -> &str {
@@ -45,13 +45,16 @@ trait State {
     fn content<'a>(&self, _post: &'a Post) -> &'a str {
         ""
     }
+    fn add_text(&self, current_text: &str, _text: &str) -> String {
+        current_text.to_string()
+    }
 }
 
 struct Draft {}
 
 impl State for Draft {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
-        Box::new(PendingReview {})
+        Box::new(PendingReview { approvals: 0 })
     }
 
     fn approve(self: Box<Self>) -> Box<dyn State> {
@@ -61,17 +64,28 @@ impl State for Draft {
     fn draft(self: Box<Self>) -> Box<dyn State> {
         self
     }
+    fn add_text(&self, current_text: &str, text: &str) -> String {
+        format!("{}{}", text, current_text)
+    }
 }
 
-struct PendingReview {}
+struct PendingReview {
+    approvals: u8,
+}
 
 impl State for PendingReview {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
         self
     }
 
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Published {})
+    fn approve(mut self: Box<Self>) -> Box<dyn State> {
+        self.approvals += 1;
+
+        if self.approvals >= 2 {
+            return Box::new(Published {});
+        } else {
+            self
+        }
     }
 
     fn draft(self: Box<Self>) -> Box<dyn State> {
@@ -124,6 +138,7 @@ mod test {
         post.request_review();
         assert_eq!("", post.content());
 
+        post.approve();
         post.approve();
         assert_eq!("I ate a salad for lunch today", post.content());
     }
