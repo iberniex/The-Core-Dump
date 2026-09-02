7da68453592adb2e7dfe7a119d0598ffd0d898f3
iberniex <ben.karanja@icloud.com>
2026-09-02 17:10:35 +0300

    feat(rust): adding reference

--- Experiments/rust/src/bin/static_oopsies.rs
+++ Experiments/rust/src/bin/static_oopsies.rs
@@ -3,6 +3,7 @@ use std::thread;
 fn main() {
     /// Never use mutable static values they live for entirety
     /// of the program call
+    /// WARNING: https://doc.rust-lang.org/edition-guide/rust-2024/static-mut-references.html
     static mut STATIC: i32 = 55;
 
     thread::spawn(move || unsafe {
