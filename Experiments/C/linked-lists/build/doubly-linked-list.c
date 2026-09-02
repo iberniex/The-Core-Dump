1f7805cc03b4d29c5b35cf7caff65e0d576bf6b1
iberniex <ben.karanja@icloud.com>
2026-09-02 11:08:17 +0300

    feat(C): working in creating a doubly-linked list #TODO

---
+++ Experiments/C/linked-lists/build/doubly-linked-list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+struct Node {
+  int data;
+  struct Node *previous;
+  struct Node *next;
+} Node;
+
+int main(int argc, char *argv[]) {
+
+  char *ready = malloc(40);
+
+  struct Node *start;
+  {
+    int a = 1;
+    0;
+    0;
+  };
+
+  struct Node end;
+  int *start.next = &end;
+
+  return EXIT_SUCCESS;
+}
